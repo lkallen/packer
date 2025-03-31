d41253e02840e3c4b3640ea3c2af7fe269c6ce59
Lindsay Allen <159066551+lkallen@users.noreply.github.com>
2025-03-31 18:05:56 -0400

    auth gone

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,9 @@
-import { SignInButton } from "@clerk/nextjs";
 
 export default async function Home() {
   return (
     <div>
       <h1>home</h1>
-      <SignInButton
-       
-        initialValues={{
-          username: "guest",
-        }}
-      />
+    
     </div>
   );
 }
